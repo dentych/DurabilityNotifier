c6b956c137d1f209ff64598a57ee10110b322cd0
Dennis <dennis@dennis-t.dk>
2014-04-01 15:46:33 +0200

    Test change

--- src/dk/dennist/durnotifier/Main.java
+++ src/dk/dennist/durnotifier/Main.java
@@ -9,7 +9,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 /*
  * ----------------------------------------------------------------------------
- * "THE BEER-WARE LICENSE" (Revision 42):
+ * "THE BEER-WARE LICENSE" (Revision 42): (test)
  * <dentych@github> wrote this file. As long as you retain this notice you
  * can do whatever you want with this stuff. If we meet some day, and you think
  * this stuff is worth it, you can buy me a beer in return Dennis T
